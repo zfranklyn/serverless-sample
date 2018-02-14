38431cb9f75e6465a839ad43b1d5a327c781a3a4
Franklyn Zhu <zfranklyn@gmail.com>
2018-02-15 01:54:18 +0800

    init

---
+++ api/libs/dynamodb-lib.js
@@ -0,0 +1,10 @@
+import AWS from 'aws-sdk';
+
+AWS.config.update({ region: 'ap-northeast-2' });
+
+export function call(action, params) {
+	const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+	return dynamoDb[action](params).promise();
+
+}
---
+++ ui/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <input/>
+          <input/>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
